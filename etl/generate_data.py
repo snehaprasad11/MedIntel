1f347905bcab2400155e32181ae8a3b456c6a7ad
snehaprasad11 <snehaprasad117@gmail.com>
2026-06-20 17:07:58 +0530

    Generated doctor and department datasets

--- etl/generate_data.py
+++ etl/generate_data.py
@@ -24,3 +24,54 @@ patients_df.to_csv(
 )
 
 print("Patients generated")
+
+departments = []
+
+for department_id in range(1, 51):
+
+    departments.append({
+        "department_id": department_id,
+        "department_name": f"Department_{department_id}"
+    })
+
+departments_df = pd.DataFrame(departments)
+
+departments_df.to_csv(
+    "data/synthetic/departments.csv",
+    index=False
+)
+
+print("Departments generated")
+
+specialties = [
+    "Cardiology",
+    "Neurology",
+    "Orthopedics",
+    "Pediatrics",
+    "Emergency",
+    "Dermatology",
+    "Oncology",
+    "Radiology",
+    "General Medicine",
+    "ENT"
+]
+
+doctors = []
+
+for doctor_id in range(1, 501):
+
+    doctors.append({
+        "doctor_id": doctor_id,
+        "doctor_name": fake.name(),
+        "specialty": random.choice(specialties),
+        "department_id": random.randint(1, 50)
+    })
+
+doctors_df = pd.DataFrame(doctors)
+
+doctors_df.to_csv(
+    "data/synthetic/doctors.csv",
+    index=False
+)
+
+print("Doctors generated")
